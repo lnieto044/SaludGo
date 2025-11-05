2c4072ad90b6c1a192d2ad0340e4cdcd6edc9d60
lnieto <guillermonieto.2003@gmail.com>
2025-11-04 23:11:33 -0500

    Initial commit

--- app.py
+++ app.py
@@ -1575,4 +1575,4 @@ def generate_chatbot_reply(text: str) -> str:
 if __name__ == "__main__":
     with app.app_context():
         init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
